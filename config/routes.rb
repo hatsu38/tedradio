71f367d20da64fa75ddebd59fc47169125b74c03
toywonder <toy.wonder70@gmail.com>
2018-09-27 16:38:43 +0900

    speakerページの作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-
   root to: "talks#index"
   resources :talks, only: [:index,:show]
   resources :categories, only: [:index,:show]
   resources :subcategories, only: [:show]
+  resources :speakers, only: [:index,:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
